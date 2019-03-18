279a1f0f2f42a92408d554a672a5e3c513cdcff0
Steve Kahn <steve@kahn.org.uk>
2019-03-18 15:59:26 +0000

    Fixed typo in note-model-test

--- tests/note-model-test.js
+++ tests/note-model-test.js
@@ -1,6 +1,6 @@
 describe("Note", function(){
   it("test it stores text",function() {
       note = new Note("My favourite language is JS");
-      assert.isEqual(note.text, "My favourite language is J");
+      assert.isEqual(note.text, "My favourite language is JS");
   });
 });
