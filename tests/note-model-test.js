eb862982ae52d3bb40935f9196ebf9b667bddc14
Steve Kahn <steve@kahn.org.uk>
2019-03-18 15:58:40 +0000

    Created EagleSpec

--- tests/note-model-test.js
+++ tests/note-model-test.js
@@ -1,14 +1,6 @@
-var assert = {
-    isEqual: function(testObj, expectedResult) {
-        if (!(testObj === expectedResult)) {
-            throw new Error(testObj + " is not equal to " + expectedResult)
-        }
-    }
-}
-
-function testItStoresText() {
-    note = new Note("My favourite language is JS");
-    assert.isEqual(note.text, "My favourite language is JS");
-}
-
-testItStoresText();
+describe("Note", function(){
+  it("test it stores text",function() {
+      note = new Note("My favourite language is JS");
+      assert.isEqual(note.text, "My favourite language is J");
+  });
+});
