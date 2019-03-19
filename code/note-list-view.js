d97c7c2f95416504a88dbf4964903ceb98a7a1fe
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-19 15:23:47 +0000

    pass test for notelistview giving html for 1 note in list

--- code/note-list-view.js
+++ code/note-list-view.js
@@ -3,7 +3,15 @@
    this.list = list
   }
   NoteListView.prototype.getHTML = function() {
-   return ""
+   var notes = this.list.notes
+   html = ""
+   for (i = 0; i < notes.length; i++){
+    html += "<li><div>" + notes[i].getText() + "</div></li>"
+   }
+   if (notes.length > 0) {
+       html = "<ul>" + html + "</ul>"
+   }
+   return html
   }
 
 exports.NoteListView = NoteListView
--- tests/note-list-view-test.js
+++ tests/note-list-view-test.js
@@ -5,4 +5,13 @@ describe('noteListView', function() {
      assert.isEqual(notelistview.getHTML(), "")
   });
 
+  it('returns an html list with 1 list item when the NoteList has one Note', function() {
+    notelist = new NoteList()
+    note1 = new Note("my first note")
+    notelist.addNote(note1)
+    view = new NoteListView(notelist)
+    html1 = "<ul><li><div>my first note</div></li></ul>"
+    assert.isEqual(view.getHTML(), html1)
+  })
+
 });
