266b24dd4b75c54319c2190cbe99b0286be6c2bc
James Ault <james.ault@johnlewis.co.uk>
2019-03-20 15:50:18 +0000

    Passing test for Note IDs

--- code/note-list-model.js
+++ code/note-list-model.js
@@ -1,8 +1,11 @@
 (function(exports) {
     function NoteList() {
         this.notes = [];
+        this.id = 0
     }
     NoteList.prototype.addNote = function (note) {
+      note.id = this.id
+      this.id += 1
       this.notes.push(note)
     };
     exports.NoteList = NoteList;
--- tests/note-list-model-test.js
+++ tests/note-list-model-test.js
@@ -10,4 +10,18 @@ describe("NoteList", function() {
       list.addNote(note)
       assert.contains(list.notes, note)
     })
+    it("a note id is added when adding to the list", function(){
+      list = new NoteList;
+      note = new Note('This is a note')
+      list.addNote(note)
+      assert.isEqual(list.notes[0].id, 0)
+    })
+    it("The note id is incremented as each note is added to the list", function(){
+      list = new NoteList;
+      note = new Note('This is a note')
+      note2 = new Note('This is another note')
+      list.addNote(note)
+      list.addNote(note2)
+      assert.isEqual(list.notes[1].id, 1)
+    })
 });
